aea7a14c9ddd9ea4912121e3af585581f32976a6
Gloom-design <ihmfeporsky@gmail.com>
2026-09-22 12:46:20 +0800

    feat: move portfolio summary to top of tab views below tab switcher and bump to v0.2.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// CostCal Service Worker - Version v0.2.1
-const SW_VERSION = 'v0.2.1';
+// CostCal Service Worker - Version v0.2.2
+const SW_VERSION = 'v0.2.2';
 const CACHE_NAME = 'costcal-pwa-' + SW_VERSION;
 const urlsToCache = [
   './',
