cc42fc1cd407dc0dbbb6faca723dea14d268d46f
Gloom-design <ihmfeporsky@gmail.com>
2026-09-23 12:50:42 +0800

    chore(release): bump version to V0.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// CostCal Service Worker - Version v0.6.1
-const SW_VERSION = 'v0.6.1';
+// CostCal Service Worker - Version V0.0
+const SW_VERSION = 'V0.0';
 const CACHE_NAME = 'costcal-pwa-' + SW_VERSION;
 const urlsToCache = [
   './',
